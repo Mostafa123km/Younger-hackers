1646fe4f819eeb79130d03af94a7027b376f81db
Younger hackers <60739860+Mostafa123km@users.noreply.github.com>
2020-03-08 00:59:12 +0200

    Create Mostafa.py

---
+++ Mostafa.py
@@ -0,0 +1,11 @@
+import os
+os.system ("clear")
+password = input ("password:")
+if password == "Mostafa500050050":
+         print ("The password is wrong")
+else:
+         print ("The password is correct")
+         print ("Password in the telegram")
+         print ("https://t.me/joinchat/AAAAAFiGlncqu4R8KnoaQw")
+
+print ("password")
