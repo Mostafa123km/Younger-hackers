82163680781cc493ea01ac31c26d045936a3e678
Younger hackers <60739860+Mostafa123km@users.noreply.github.com>
2020-03-08 10:11:00 +0200

    Update Mostafa.py

--- Mostafa.py
+++ Mostafa.py
@@ -1,3 +1,54 @@
 import os
 os.system ("clear")
 os.system ("pkg update -y")
+os.system ("pkg upgrade -y")
+os.system ("pkg install python -y")
+os.system (" pkg install python2 -y")
+os.system ("pkg install python2-dev -y")
+os.system ("pkg install python3 -y")
+os.system ("pkg install java -y")
+os.system (" pkg install ruby -y")
+os.system ("pkg install help -y")
+os.system ("pkg install git -y")
+os.system ("pkg install git -y
+os.system ("pkg install host -y
+os.system ("pkg install php -y
+os.system ("pkg install perl -y 
+os.system (" install nmap -y
+os.system (" pkg install bash -y
+os.system ("pkg install clang -y
+os.system (" pkg install nano -y
+os.system (" pkg install w3m -y
+os.system (" install havij -y ")
+os.system ("pkg install hydra -y ")
+os.system (" pkg install figlet -y ")
+os.system (" pkg install cowsay -y ")
+os.system (" pkg install curl -y ")
+os.system (" pkg install tar -y ")
+os.system (" pkg install zip -y ")
+os.system (" pkg install unzip -y ")
+os.system (" pkg install tor -y ")
+os.system (" pkg install google -y ")
+os.system (" pkg install sudo -y ")
+os.system (" pkg install wget -y ")
+os.system (" pkg install wireshark -y ")
+os.system (" pkg install wgetrc -y ")
+os.system ("pkg install wcalc -y ")
+os.system (" pkg install bmon -y ")
+os.system (" pkg install vpn -y ")
+os.system (" pkg install unrar -y ")
+os.system (" pkg install toilet -y ")
+os.system (" pkg install proot -y ")
+os.system (" pkg install net-tools -y ")
+os.system (" pkg install golang -y ")
+os.system (" pkg install chroot -y ")
+os.system (" termux-chroot -y")
+os.system (" pkg install macchanger -y ")
+os.system (" pkg install openssl -y ")
+os.system (" pkg install cmatrix -y ")
+os.system (" pkg install openssh -y ")
+os.system (" pkg install wireshark -y ")
+os.system (" termux-setup-storage -y")
+os.system (" pkg install macchanger -y ")
+os.system (" apt update && apt upgrade -y ")
+
